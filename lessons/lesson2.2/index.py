28c37337239184e90453ae0dbfe331c9227fe7a2
arxip@inbox.ru <Poiskeone03>
2018-03-27 16:21:17 +0300

    add some

---
+++ lessons/lesson2.2/index.py
@@ -0,0 +1,7 @@
+arr = ['newsafr.txt', 'newscy.txt', 'newsfr.txt', 'newsit.txt'] #список файлов с новостями
+
+for doc in arr:
+    print(doc)    
+    # with open(doc) as file:
+    #     read = file.read()
+    #     prinr(read)
