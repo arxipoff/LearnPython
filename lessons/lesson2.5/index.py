4a7b70836169ea95a9e649cb24138ad5b6902186
arxip@inbox.ru <Poiskeone03>
2018-04-05 13:41:33 +0300

    home_work_2.5

--- lessons/lesson2.5/index.py
+++ lessons/lesson2.5/index.py
@@ -1,21 +1,39 @@
-# import os
-
-# print(os.environ)
-# print('Hello', os.environ['USER'])
-# os.environ['TOKEN'] = 'secure token'
-# print('TOKEN: ', os.environ['TOKEN'])
-
-# for name, value in os.environ.items():
-#     print('{}: {}'.format(name, value))
-
-
+import os
 import sys
+import subprocess
 
-print('This is stdout', file=sys.stdout)
-print('This is stderr', file=sys.stderr)
 
-with open('file.log', mode='w') as f:
-    print('this is code in file', file=f)
+def check_result_folder():
+    if os.path.exists(os.path.join(os.getcwd(), "Result")):
+        print("Папки Result нет")
+    else:
+        print("Папки Result нет, создааем папку Result")
+        subprocess.call(["mkdir", "Result"])
 
-s = input()
-print(s)    
+
+def copy_file_to_result():
+    file_list = os.listdir(os.path.join(os.getcwd(), "Source"))
+
+    for file in file_list:
+        file_to_copy = (os.path.join(os.getcwd(), "Source", file))
+        subprocess.call(["cp", file_to_copy, "Result"])
+        print("Скопирован файл:", file)
+
+
+def convert_imgs():
+    for file in os.listdir("Result"):
+        file_to_convert = (os.path.join(os.getcwd(), "Result", file))
+        print("Конвертируем файл:", file)
+        subprocess.call(["convert", file_to_convert, "-resize", "200", file_to_convert])
+
+
+
+
+def convert_img():
+    check_result_folder()
+    copy_file_to_result()
+    convert_imgs()
+
+
+
+convert_img()
